5e75bd75b9fd0ed1ccc34c8d1490c20dbfcb2def
VIYD <viyd@i.ua>
2023-10-03 14:32:47 +0300

    second prototype, not related to theme

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ function displayMessage() {
     messageElement.textContent = 'Hello, soup!';
 }
 
+function changeTitle(id) {
+    const titleElement = document.getElementById(id);
+    titleElement.title = 'інший тултіп!';
+}
+
 // Add a click event listener to the button
 const button = document.getElementById('displayButton');
 button.addEventListener('click', displayMessage);
+
+const paragraph = document.getElementById("p1");
+paragraph.addEventListener('click', function() {
+    changeTitle("p1");
+});
+
