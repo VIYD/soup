cdc784876fd902fa6984083a2856e48bfe3b79a9
VIYD <viyd@i.ua>
2023-10-11 15:47:36 +0300

    backend doesnt work; need http?

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-var mysql = require("mysql");
+const mysql = require("mysql");
 
 var db = mysql.createConnection({
     host: 'localhost',
@@ -15,14 +15,39 @@ db.connect((err) => {
     console.log('Connected to the database');
 });
 
-db.query('SELECT * FROM card_1;', (err, results, fields) => {
-    if (err) {
-        console.error('Error executing query:', err);
-        return;
-    }
-    console.log('Query results:', results);
+const loadButton = document.getElementById("load-button");
+const saveButton = document.getElementById("save-button");
+const cardTitle = document.querySelector(".card-title");
+const cardDescription = document.querySelector(".card-description");
+
+loadButton.addEventListener("click", () => {
+    db.query('SELECT * FROM card_1', (err, results, fields) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            return;
+        }
+        
+        console.log('Data received from the database');
+        if (results.length > 0) {
+            const data = results[0];
+            cardTitle.value = data.title;
+            cardDescription.value = data.description;
+        }
+    });
 });
 
+saveButton.addEventListener("click", () => {
+    const title = cardTitle.value;
+    const description = cardDescription.value;
+    
+    db.query('INSERT INTO card_1 (title, description) VALUES (?, ?)', [title, description], (err, results, fields) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            return;
+        }
+        console.log('Data saved successfully');
+    });
+});
 
 process.on('SIGINT', () => {
     db.end();
