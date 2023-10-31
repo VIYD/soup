817d6afad8cce6a73383ff62b3193acab851b887
VIYD <viyd@i.ua>
2023-10-31 20:04:03 +0200

    reverting

--- server/index.js
+++ server/index.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
-
-const port = 3001;
+const port = 3000;
 
 app.use(express.json());
-app.use(cors());
 
 const db = require('../models');
 
