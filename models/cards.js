40e73720765064e86a42fd08f70abb7582700fff
VIYD <viyd@i.ua>
2023-11-04 13:58:31 +0200

    db: renamed essential files

---
+++ models/cards.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+    const Cards = sequelize.define("cards", {
+        title : {
+            type: DataTypes.STRING,
+        },
+        description : {
+            type: DataTypes.STRING,
+        },
+    });
+
+    //todo: зробити таблицю з коментарями та імплементувати систему коментарів
+    //
+
+    // Cards.associate = (models) => {
+    //     Cards.hasMany(models.Comments, {
+    //         foreignKey: 'commentID',
+    //         onDelete: 'cascade',
+    //     })
+    // }
+    
+    return Cards
+}
