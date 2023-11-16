ad4f2d3310bc53122192871fd661fb65412e3352
VIYD <viyd@i.ua>
2023-11-16 21:07:13 +0200

    feat: deleting cards is possible, minor ui changes

--- client/src/pages/ChosenCardPage.js
+++ client/src/pages/ChosenCardPage.js
@@ -1,29 +1,78 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+import { AuthContext } from "../helpers/AuthContext";
 
 function Card() {
   let { id } = useParams();
+  let navigate = useNavigate();
   const [cardObject, setCardObject] = useState([]);
 
+  let [authState, setAuthState] = useState({
+    username: "",
+    id: 0,
+    isLogged: false,
+  });
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/auth/checkAuth", {
+        headers: {
+          accessToken: localStorage.getItem("accessToken"),
+        },
+      })
+      .then((response) => {
+        if (response.data.error) {
+          setAuthState({ ...authState, isLogged: false });
+        } else {
+          setAuthState({
+            username: response.data.username,
+            id: response.data.id,
+            isLogged: true,
+          });
+        }
+      });
+  }, []);
+
   useState(() => {
-    //useEffect
     axios.get(`http://localhost:3001/cards/byID/${id}`).then((response) => {
       setCardObject(response.data);
     });
   });
 
+  const deleteCard = (id) => {
+    axios
+      .delete(`http://localhost:3001/cards/${id}`, {
+        headers: {
+          accessToken: localStorage.getItem("accessToken"),
+        },
+      })
+      .then(() => navigate("/"));
+  };
+
   return (
     <div className="cardPage">
-      <div className="cardChosen">
-        <div className="titleChosen">{cardObject.title}</div>
-        <textarea
-          className="descriptionChosen"
-          readOnly
-          value={cardObject.description}
-        ></textarea>
-        <div className="usernameChosen">Created by {cardObject.username}</div>
-      </div>
+      <AuthContext.Provider value={{ authState, setAuthState }}>
+        <div className="cardChosen">
+          <div className="titleChosen">{cardObject.title}</div>
+          <textarea
+            className="descriptionChosen"
+            readOnly
+            value={cardObject.description}
+          ></textarea>
+          <div className="usernameChosen">Created by {cardObject.username}</div>
+        </div>
+        {authState.username === cardObject.username && (
+          <button
+            className="deleteButton"
+            onClick={() => deleteCard(cardObject.id)}
+          >
+            Delete
+          </button>
+        )}
+      </AuthContext.Provider>
     </div>
   );
 }
--- server/routes/cards.js
+++ server/routes/cards.js
@@ -1,25 +1,35 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { cards } = require('../../models');
-const { validateToken } = require('../middleware/TokenMiddleware');
+const { cards } = require("../../models");
+const { validateToken } = require("../middleware/TokenMiddleware");
 
-router.get('/', async (req, res) => {
-   const listOfCards = await cards.findAll();
-   res.json(listOfCards);
+router.get("/", async (req, res) => {
+  const listOfCards = await cards.findAll();
+  res.json(listOfCards);
 });
 
-router.get('/byID/:id', async (req, res) => {
-    const id = req.params.id;
-    const card = await cards.findByPk(id);
-    res.json(card);
+router.get("/byID/:id", async (req, res) => {
+  const id = req.params.id;
+  const card = await cards.findByPk(id);
+  res.json(card);
 });
 
-router.post('/', validateToken, async (req, res) => {
-    const card = req.body;
-    const username = req.user.username;
-    card.username = username;
-    const createdCard = await cards.create(card);
-    res.json({ id: createdCard.id });
+router.post("/", validateToken, async (req, res) => {
+  const card = req.body;
+  const username = req.user.username;
+  card.username = username;
+  const createdCard = await cards.create(card);
+  res.json({ id: createdCard.id });
+});
+
+router.delete("/:id", validateToken, async (req, res) => {
+  const cardId = req.params.id;
+  await cards.destroy({
+    where: {
+      id: cardId,
+    },
+  });
+  res.send(200);
 });
 
 module.exports = router;
